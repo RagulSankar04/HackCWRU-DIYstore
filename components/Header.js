57dc82857096ecd3d34d55866b43df4cea51da17
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-03-19 22:18:08 +0530

    1:00pm Commit

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,39 @@
-import { StyleSheet, View, SafeAreaView } from "react-native";
+import { StyleSheet, View, SafeAreaView, Text } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Avatar, Text } from "react-native-elements";
+import { Avatar } from "react-native-elements";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <SafeAreaView style={styles.conatiner}>
-      <StatusBar style="dark" />
       <View style={styles.navbar}>
-        <Avatar
-          size={130}
-          source={require("../images/logo.png")}
-          style={styles.avatar}
-          key={0}
-        />
+        <Avatar size={50} source={require("../images/logo.png")} key={0} />
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 20,
+            borderWidth: 2,
+            borderColor: "grey",
+            padding: 4,
+            borderRadius: 12,
+            paddingLeft: 8,
+          }}
+        >
+          {props.displayName}
+        </Text>
+        {props.photoURL ? (
+          <Avatar size={50} source={{ uri: props.photoURL }} rounded key={0} />
+        ) : (
+          <Avatar
+            size={50}
+            rounded
+            containerStyle={{ backgroundColor: "grey" }}
+            title={props.displayName[0]}
+            key={0}
+          />
+        )}
       </View>
+      <StatusBar style="dark" />
     </SafeAreaView>
   );
 };
@@ -26,13 +45,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderBottomColor: "#000",
     width: "100%",
-    height: 130,
+    height: 110,
+    zIndex: 100,
   },
-  navbar: { marginTop: 60, marginHorizontal: 20 },
+  navbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
   avatar: {
-    width: 50,
-    height: 50,
     borderWidth: 1.2,
     borderColor: "whitesmoke",
+    backgroundColor: "whitesmoke",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,34 @@
 import { StyleSheet, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Text } from "react-native-elements";
 import { auth } from "../firebase";
 import { StatusBar } from "expo-status-bar";
 import Header from "../components/Header";
 
 const HomeScreen = ({ navigation }) => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    auth.onAuthStateChanged((u) => {
+      if (u) {
+        setUser(u);
+        console.log(u);
+      }
+    });
+  }, [user]);
+
   return (
     <View>
-      <Header />
-      <StatusBar style="light" />
+      {user ? (
+        <Header
+          displayName={user.displayName || ""}
+          photoURL={user.photoURL || ""}
+        />
+      ) : (
+        <Text>Loading...</Text>
+      )}
+
+      <StatusBar style="dark" />
     </View>
   );
 };
